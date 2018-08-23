897f0a40c6c039e2d448e2d4df3dd0d9e553670c
Remi Geny-Dumont <remi.genydumont@gmail.com>
2018-08-23 11:12:02 +0200

    adding log class

---
+++ src/main/java/console/App.java
@@ -0,0 +1,18 @@
+package console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import service.AppService;
+
+public class App {
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implémentation Logback");
+		new AppService().executer("valeur 1");
+
+	}
+
+}
